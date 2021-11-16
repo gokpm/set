49cdf4be57eb00a04cbdd9d80997ee0743803d73
Gokul PM <mail@gokulpm.com>
2021-11-16 23:17:04 +0530

    union and intersection

--- set.go
+++ set.go
@@ -1,42 +1,52 @@
 package set
 
-type Set []int
-
 func Convert(numArr []int) []int {
+	setMap := map[int]bool{}
 	setArr := []int{}
-	for _, i := range numArr {
-		flag := false
-		for _, j := range setArr {
-			if i == j {
-				flag = true
-			}
+	for _, value := range numArr {
+		setMap[value] = true
+	}
+	for key, _ := range setMap {
+		setArr = append(setArr, key)
+	}
+	return setArr	
+}
+
+func Union(numMat ...[]int) []int {
+	setMap := map[int]bool{}
+	setArr := []int{}
+	for _, row := range numMat {
+		for _, value := range row {
+			setMap[value] = true
 		}
-		if flag {
-			continue
-		} else {
-			setArr = append(setArr, i)
-		}
+	}
+	for key, _ := range setMap {
+		setArr = append(setArr, key)
 	}
 	return setArr
 }
 
-func Union(numMatrix ...[]int) []int {
+func Intersection(numMat ...[]int) []int {
+	setMap := map[int]bool{}
 	setArr := []int{}
-	for _, i := range numMatrix {
-		for _, j := range i {
-			flag := false
-			for _, z := range setArr {
-				if z == j {
-					flag = true
-				}
-			if flag {
+	for _, value := range numMat[0] {
+		setMap[value] = true
+	}
+	numMat = numMat[1:]
+	for _, row := range numMat {
+		tmpMap := map[int]bool{}
+		for _, value := range row{
+			tmpMap[value] = true
+		}
+		for key, _ := range setMap {
+			if !tmpMap[key] {
+				delete(setMap, key)
 				continue
-			} else {
-				setArr = append(setArr, j)
-			}
 			}
 		}
 	}
+	for key, _ := range setMap {
+		setArr = append(setArr, key)
+	}
 	return setArr
 }
-
